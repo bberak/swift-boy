aff095edf2e1cfc1fd103c876c01a951b02eff8d
bberak <bberak@gmail.com>
2021-08-10 17:31:02 +1000

    Some cleanup

--- Swift Boy.playground/Contents.swift
+++ Swift Boy.playground/Contents.swift
@@ -4,20 +4,5 @@ let cartridge = Cartridge(path: #fileLiteral(resourceName: "cpu_instrs.gb"), tit
 let mmu = MMU(cartridge: cartridge)
 let cpu = CPU(mmu: mmu)
 
-
-func printInstruction(op: UInt8) {
-print(
-"""
-    OpCode.word(0x\(op.toHexString())): Instruction.atomic(cycles: 2) { cpu in
-        // Add comment here
-        throw CPUError.instructionNotImplemented(OpCode.word(0x\(op.toHexString())))
-    },
-""")
-}
-
-for i in 0x00...0xFF {
-    printInstruction(op: UInt8(i))
-}
-
-//try cpu.start()
+try cpu.start()
 
--- Swift Boy.playground/Sources/Instructions.swift
+++ Swift Boy.playground/Sources/Instructions.swift
@@ -1,13 +1,15 @@
 let instructions: [OpCode: Instruction] = [
     OpCode.byte(0x00): Instruction.atomic(cycles: 1) { cpu in
-        // Only advances the program counter by 1. Performs no other operations that would have an effect.
+        // Only advances the program counter by 1. Performs no other operations
+        // that would have an effect.
     },
     OpCode.byte(0x01): Instruction.atomic(cycles: 3) { cpu in
         // Load the 2 bytes of immediate data into register pair BC.
         cpu.bc = try cpu.readNextWord()
     },
     OpCode.byte(0x02): Instruction.atomic(cycles: 3) { cpu in
-        // Store the contents of register A in the memory location specified by register pair BC.
+        // Store the contents of register A in the memory location specified by
+        // register pair BC.
         try cpu.mmu.writeByte(address: cpu.bc, byte: cpu.a)
     },
     OpCode.byte(0x03): Instruction.atomic(cycles: 2) { cpu in
@@ -35,10 +37,11 @@ let instructions: [OpCode: Instruction] = [
         cpu.b = try cpu.readNextByte()
     },
     OpCode.byte(0x07): Instruction.atomic(cycles: 1) { cpu in
-        // Rotate the contents of register A to the left. That is, the contents of bit 0 are copied
-        // to bit 1, and the previous contents of bit 1 (before the copy operation) are copied to bit 2.
-        // The same operation is repeated in sequence for the rest of the register. The contents of bit 7
-        // are placed in both the CY flag and bit 0 of register A.
+        // Rotate the contents of register A to the left. That is, the contents of
+        // bit 0 are copied to bit 1, and the previous contents of bit 1
+        // (before the copy operation) are copied to bit 2. The same operation is repeated
+        // in sequence for the rest of the register. The contents of bit 7 are placed
+        // in both the CY flag and bit 0 of register A.
         let carry = cpu.a.bit(7)
         cpu.a = (cpu.a << 1) + (carry ? 1 : 0)
         cpu.flags.carry = carry
@@ -712,8 +715,8 @@ let instructions: [OpCode: Instruction] = [
         throw CPUError.instructionNotImplemented(OpCode.byte(0xAE))
     },
     OpCode.byte(0xAF): Instruction.atomic(cycles: 1) { cpu in
-        // Take the logical exclusive-OR for each bit of the contents of register A and the contents
-        // of register A, and store the results in register A.
+        // Take the logical exclusive-OR for each bit of the contents of register A
+        // and the contents of register A, and store the results in register A.
         cpu.a^=cpu.a
         cpu.flags.zero = cpu.a == 0
     },
