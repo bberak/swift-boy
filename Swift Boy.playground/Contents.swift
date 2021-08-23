19101de48474daba974580fcbfa8d4458722fe0a
bberak <bberak@gmail.com>
2021-08-23 23:36:05 +1000

    Started working on a simple screen implementation

--- Swift Boy.playground/Contents.swift
+++ Swift Boy.playground/Contents.swift
@@ -1,11 +1,17 @@
 import Foundation
+import PlaygroundSupport
 
 let cart = Cartridge(path: #fileLiteral(resourceName: "cpu_instrs.gb"), title: "Blargg CPU Test")
 let mmu = MMU()
 let ppu = PPU(mmu)
 let cpu = CPU(mmu, ppu)
 
-try cpu.start()
+try? cpu.start()
+
+PlaygroundPage.current.setLiveView(ppu.screen)
+
+
+
 
 
 
--- Swift Boy.playground/Sources/PPU.swift
+++ Swift Boy.playground/Sources/PPU.swift
@@ -1,7 +1,21 @@
 import Foundation
+import SwiftUI
 
 public class PPU {
     private let mmu: MMU
+    public var screen: some View {
+        VStack(spacing: 0) {
+            ForEach(1...144, id: \.self) { _ in
+                HStack(spacing: 0) {
+                    ForEach(1...160, id: \.self) { _ in
+                        Rectangle()
+                            .foregroundColor(Color.gray)
+                            .frame(width: 1, height: 1)
+                    }
+                }
+            }
+        }
+    }
     
     public init(_ mmu: MMU) {
         self.mmu = mmu
