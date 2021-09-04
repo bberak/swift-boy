6bf69f25d0729528a819424588ab891d6a926032
bberak <bberak@gmail.com>
2021-09-04 12:25:36 +1000

    Added a better bios program

--- Swift Boy.playground/Contents.swift
+++ Swift Boy.playground/Contents.swift
@@ -2,7 +2,7 @@ import Foundation
 import PlaygroundSupport
 
 let cart = Cartridge(path: #fileLiteral(resourceName: "cpu_instrs.gb"), title: "Blargg's CPU Test")
-let mmu = MMU()
+let mmu = MMU(cart)
 let ppu = PPU(mmu)
 let cpu = CPU(mmu, ppu)
 let clock = Clock(mmu, ppu, cpu)
