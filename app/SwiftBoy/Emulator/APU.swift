b370f5b488203cbecbfd98f7c80c44dbd4f16872
Boris Berak <bberak@gmail.com>
2022-05-15 12:21:32 +1000

    Updated comments

--- app/SwiftBoy/Emulator/APU.swift
+++ app/SwiftBoy/Emulator/APU.swift
@@ -2,6 +2,7 @@
 // TODO: A weird noise can be heard as soon as the GB is turned on
 // TODO: Super Mario sounds and music are completely off
 // TODO: Tetris main menu and Type-B music is not sounding correct
+// TODO: Still need to implement the noise voice properly
 
 import Foundation
 import AudioKit
